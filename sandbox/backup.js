1e03546412fc7d8c0e1b5927c511e45044f571c4
GranygamerCB2 <tbug0510@gmail.com>
2024-10-08 11:25:26 -0500

    Create backup.js

---
+++ sandbox/backup.js
@@ -0,0 +1,17 @@
+
+
+
+
+<script type="text/javascript" src="../build/firebug-lite-beta.js#debug"></script>
+<script type="text/javascript">
+(function(){
+var console = window.firebug || window.console;
+
+console.log("This is a test call to console.log()");
+console.info("This is a test call to console.info()");
+console.warn("This is a test call to console.warn()");
+console.error("This is a test call to console.error()");
+})();
+</script>
+</head>
+</html>
